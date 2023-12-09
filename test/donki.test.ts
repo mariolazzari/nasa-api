24fac77a52286a48843eac72777e32a1103f8af3
Mario Lazzari <mario.lazzari@gmail.com>
2023-12-09 18:51:16 +0100

    refact: donki gst

--- test/donki.test.ts
+++ test/donki.test.ts
@@ -39,4 +39,14 @@ describe('DONKI', () => {
     expect(error).toBeUndefined();
     expect(data).toBeDefined();
   });
+
+  it('should return last 30 days Geomagnetic Storm (GST)', async () => {
+    const from = new Date(2020, 0, 1);
+    const to = new Date(2020, 0, 31);
+    const { success, error, data } = await nasa.donkiGst(from, to);
+
+    expect(success).toBeTruthy();
+    expect(error).toBeUndefined();
+    expect(data).toBeDefined();
+  });
 });
